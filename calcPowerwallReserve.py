61bd52c0fa01574e80b4d8fb982a91be9d8215b0
Steven Owen <steven.owen@simos-iot.com>
2021-04-11 16:50:53 +1000

    Update calcPowerwallReserve.py

--- calcPowerwallReserve.py
+++ calcPowerwallReserve.py
@@ -5,30 +5,25 @@ import datetime
 
 #   Define and set operational variables
 reserve=charge=forecast=solarForecast=shortfall=batteryReserve=0
-homeRequirements=25
+homeRequirements = 25      # This value is set to whatever your average daily power consumption is in kWh's...recommending adding +10% for contingencies.
+powerwallCapacity = 13.4   # This is the accepted capacity of a single Powerwall 2.  For multiple Powerwall 2's simply increment this value. 
 
 async def main():
     try:
         client = TeslaApiClient('YourPowerwallUsername', 'YourPowerwallPassword')
         energy_sites = await client.list_energy_sites()
         reserve = await energy_sites[0].get_backup_reserve_percent()
-        charge = await energy_sites[0].get_energy_site_live_status_percentage_charged() / 100 * 13.4
+        charge = await energy_sites[0].get_energy_site_live_status_percentage_charged() / 100 * powerwallCapacity
         await energy_sites[0].set_backup_reserve_percent(reserve+1)
 
         solarForecast=int(getSolarForecast.forecast)
-        shortfall=homeRequirements-(solarForecast+charge-1.34)
-        batteryReserve=abs(shortfall/13.4)*100
-        if batteryReserve < 10:
-                batteryReserve = 10
-
-            if batteryReserve > 100:
-                batteryReserve = 98
+        shortfall=homeRequirements-(solarForecast+charge-1.34) # The value 1.34 represents 10% of a single Powerwall 2 capacity.  This reserve is a personal choice.
 
         if shortfall > 13.4:
-            # Set reserve at 99%
+            # Tesla recommend that the Powerwall should not be charged from the grid to 100%, hence stop at 99%
             await energy_sites[0].set_backup_reserve_percent(99)
-        elif shortfall <0:
-            # Maintain reserve at 10%
+        elif shortfall <10:
+            # I prefer 10% as a minimum value following overnight charging
             await energy_sites[0].set_backup_reserve_percent(10)
         else:
             # Set reserve to calculated amount
